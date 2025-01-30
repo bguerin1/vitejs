48c0b1fea676c5921e84ca7b30059ac9dd1e6567
Bryan Guerin <bryan.guerin07@gmail.com>
2025-01-30 14:09:48 +0100

    first commit with all files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  defineProps<{ msg: string }>()
+
+  // Déclaration d'une variable « observé » de type Int
+  const count = ref(0)
+
+  // Déclaration d'une variable « observé » de type String
+  const input = ref('Votre nom')
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count--">-</button>
+    <button type="button">{{ count }}</button>
+    <button type="button" @click="count++">+</button>
+  </div>
+
+  <div class="card">
+    <h2>Hello « {{ input }} »</h2>
+    <input v-model="input"/>
+  </div>
+</template>
+
+
+
+<style scoped>
+  .card {
+    background: #595959;
+    border: 1px solid #ccc;
+    border-radius: 2px;
+    margin: 10px;
+  }
+</style>
